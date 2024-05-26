684f70a29af64ff1e82f59d27b477d7ffb7f516a
DSlayer <93089045+sharon123454@users.noreply.github.com>
2024-05-26 14:26:02 +0300

    Level parts
    Added visual representation if path is walkable.
    Created a temp map

--- Assets/BreadOnToastAssets/Scripts/Grid/PathfinidingDebugObject.cs
+++ Assets/BreadOnToastAssets/Scripts/Grid/PathfinidingDebugObject.cs
@@ -6,6 +6,7 @@ public class PathfinidingDebugObject : GridDebugObject
     [SerializeField] private TextMeshPro _gCostTMP;
     [SerializeField] private TextMeshPro _hCostTMP;
     [SerializeField] private TextMeshPro _fCostTMP;
+    [SerializeField] private SpriteRenderer _isWalkableSpriterenderer;
 
     private PathNode _pathNode;
 
@@ -15,6 +16,8 @@ public class PathfinidingDebugObject : GridDebugObject
         _gCostTMP.text = _pathNode.GetGCost().ToString();
         _hCostTMP.text = _pathNode.GetHCost().ToString();
         _fCostTMP.text = _pathNode.GetFCost().ToString();
+        if (_isWalkableSpriterenderer.gameObject.activeInHierarchy)
+            _isWalkableSpriterenderer.color = _pathNode.IsWalkable() ? Color.green : Color.red;
     }
 
     public override void SetGridObject(object gridObject)
